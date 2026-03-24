71ce62b3718659f04c7545bf7d12f630e9150c86
Codagis <codagistechnology@gmail.com>
2026-03-23 23:33:17 -0300

    feat: ajustando vercel

--- src/components/DynamicAppRoutes.jsx
+++ src/components/DynamicAppRoutes.jsx
@@ -68,7 +68,7 @@ export default function DynamicAppRoutes() {
           }
         />
       </Route>
-      <Route path="*" element={<Navigate to="/" replace />} />
+      <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   )
 }
